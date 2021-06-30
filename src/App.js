8d9efaf29f388e27d14f4bee338fe751457611e7
Sonia Chronowska <sonia@chronow.ski>
2021-06-30 06:16:01 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 
-import { Title } from "./components/Title";
-
-export const App = ({ agent = { name: "Sonia Chronowska" }, components = {} }) => {
-  const { name } = agent;
-  const { Icon } = components;
+export const App = props => {
+  const { agent, components } = props;
+  const { Table, TableBody, TableCell, TableContainer, TableRow } = components;
 
   return (
     <div>
-      <Title>Hello {name}!</Title>
-      {Icon && <Icon />}
+      <Title>Hello {agent.user.firstName}!</Title>
+      <TableContainer>
+        <Table style={{ width: "auto" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Key</TableCell>
+              <TableCell>Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.entries.map(([key, value]) => (
+              <TableRow key={key}>
+                <TableCell>key</TableCell>
+                <TableCell>{JSON.stringify(value)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
